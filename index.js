c86ffaca55b69b70f5db7e1263e59467e9bb3489
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-08-23 10:23:47 +0900

    Initialize electron app

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Main Process
+// - GUI (renderer process)
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 300,
+        title: "1min timer"
+    });
+    mainWindow.loadURL('file://' + __dirname + '/www/index.html');
+    mainWindow.on('close', function(){
+        mainWindow = null;
+    });
+});
