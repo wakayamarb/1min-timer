4e81ad76f5f15351703e60a7171f50cb472dceb6
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-08-24 14:02:59 +0900

    Make skelton source

--- README.md
+++ README.md
@@ -1,43 +1,21 @@
-# 1min timer, electron-app sample
+# 1min timer skelton
 
-[![Build Status](https://travis-ci.org/wakayamarb/1min-timer.svg?branch=master)](https://travis-ci.org/wakayamarb/1min-timer)
+example: https://wakayamarb.github.io/1min-timer/
 
-
-webView: https://wakayamarb.github.io/1min-timer/
-
-## bootstrap
+## 準備
 ```
-$ git clone --depth=1 https://github.com/wakayamarb/1min-timer.git
+$ git clone -b skelton --depth=1 https://github.com/wakayamarb/1min-timer.git
 $ npm install
 ```
 
-## preview
+## プレビュー
 ```
 $ npm start
 ```
 
-## build
-
-### asar archive
-```
-$ npm run asar
-```
-
-### desktop application
-
-#### Without Win32
-
-You can obtain Darwin(for MacOS) and linux binary.
+## ビルド
 
 ```
-$ npm run build
-```
-
-#### build Win32 app on non-windows environment
-
-You can obtain exe.
-
-```
-$ brew install wine
+$ npm run build-darwin
 $ npm run build-win32
 ```
--- www/Timer.js
+++ www/Timer.js
@@ -2,85 +2,70 @@
 // Timer class provide DOM content renderer and model of 1 second decrements.
 const Timer = class Timer {
 
-  constructor(initial, displayId, callback) {
-    this.initial = initial ? Math.floor(initial) : 60
-    this.display = document.getElementById(displayId)
-    this.callback = callback
-    this.counting = false
-    this.reset()
+  constructor(initial, displayId) {
+    // this.initial = initial ? Math.floor(initial) : 60
+    // this.display = document.getElementById(displayId)
+    // this.counting = false
+    // this.reset()
   }
 
   // start or restart timer
   start() {
-    // start can be use as reset when finished
-    if (this.value <= 0) {
-      this.reset()
-    }
-
-    // prevent duplicate call
-    if (this.counting) {
-      return
-    }
-
-    // set class attribute named 'started'
-    const classes = (this.display.getAttribute('class')).split(' ')
-      .filter(e => e != 'started')
-    classes.push('started')
-    this.display.setAttribute('class',classes.join(' '))
-
-    // timer manipulation
-    this.timerId = setInterval(
-      () => {
-        this.value -= 1
-        this.render()
-        if (this.value <= 0) {
-          this.stop()
-          if (typeof this.callback == 'function') {
-            // I want to observe render() finished in other word display.innerText changed before do callback, but not yet
-            setTimeout(this.callback, 50)
-          }
-        }
-      },
-      1000
-    )
-    this.counting = true
+    // // start can be use as reset when finished
+    // if (this.value <= 0) {
+    //   this.reset()
+    // }
+    //
+    // // prevent duplicate call
+    // if (this.counting) {
+    //   return
+    // }
+    // this.counting = true
+    //
+    // // timer manipulation
+    // this.timerId = setInterval(
+    //   () => {
+    //     this.value -= 1
+    //     this.render()
+    //     if (this.value <= 0) {
+    //       this.stop()
+    //       }
+    //     }
+    //   },
+    //   1000
+    // )
   }
 
   // stop timer
   stop() {
-    // remove class 'started'
-    const classes = (this.display.getAttribute('class')).split(' ')
-      .filter(e => e != 'started')
-    this.display.setAttribute('class',classes.join(' '))
-
-    // timer manipulation
-    clearInterval(this.timerId)
-    this.counting = false
+    // // timer manipulation
+    // clearInterval(this.timerId)
+    // this.counting = false
   }
 
   // reset model
   reset() {
-    this.stop()
-    this.timerId = false
-    this.value = this.initial
-    this.render()
+    // this.stop()
+    // this.timerId = false
+    // this.value = this.initial
+    // this.render()
   }
 
   // render dom content
   render(value) {
-    var sec = value ? value : this.value
-
-    // get hour value
-    var hour = Math.floor(sec / 3600)
-    sec -= 3600 * hour
-    //get min value
-    var min = Math.floor(sec / 60)
-    sec -= Math.round(60 * min)
-
-    // ZERO-fills
-    hour = hour < 10 ? "0" + hour : "" + hour
-    min  = min  < 10 ? "0" + min  : "" + min
-    sec  = sec  < 10 ? "0" + sec  : "" + sec
-    this.display.innerText = [hour, min, sec].join(":")
+    // var sec = value ? value : this.value
+    //
+    // // get hour value
+    // var hour = Math.floor(sec / 3600)
+    // sec -= 3600 * hour
+    // //get min value
+    // var min = Math.floor(sec / 60)
+    // sec -= Math.round(60 * min)
+    //
+    // // ZERO-fills
+    // hour = hour < 10 ? "0" + hour : "" + hour
+    // min  = min  < 10 ? "0" + min  : "" + min
+    // sec  = sec  < 10 ? "0" + sec  : "" + sec
+    // this.display.innerText = [hour, min, sec].join(":")
   }
 }
